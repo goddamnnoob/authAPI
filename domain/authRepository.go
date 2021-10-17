9be4a1efee0ec40f52f32bcbfc7d2f9b0ed78c5f
goddamnnoob <gowthamreddy547@gmail.com>
2021-10-17 13:16:57 +0530

    auth repo

--- domain/authRepository.go
+++ domain/authRepository.go
@@ -1,1 +1,31 @@
 package domain
+
+import (
+	"github.com/goddamnnoob/notReddit/errs"
+	"github.com/jmoiron/sqlx"
+)
+
+type AuthRepository interface {
+	FindBy(username string, password string) (*Login, *errs.AppError)
+}
+
+type AuthRepositoryDb struct {
+	client *sqlx.DB
+}
+
+func (d AuthRepositoryDb) FindBy(username string, password string) (*Login, *errs.AppError) {
+	var login Login
+	sqlVerify := `SELECT username, u.customer_id, role, group_concat(a.account_id) as account_numbers FROM users u
+                  LEFT JOIN accounts a ON a.customer_id = u.customer_id
+                WHERE username = ? and password = ?
+                GROUP BY a.customer_id`
+	err := d.client.Get(&login, sqlVerify, username, password)
+	if err != nil {
+		return nil, errs.NewNotFoundError("Failed Authentication")
+	}
+	return &login, nil
+}
+
+func NewAuthRepository(client sqlx.DB) AuthRepositoryDb {
+	return AuthRepositoryDb{client: &client}
+}
--- go.mod
+++ go.mod
@@ -5,4 +5,5 @@ go 1.17
 require (
 	github.com/goddamnnoob/notReddit v0.0.0-20211010052121-9adbe366d838 // indirect
 	github.com/gorilla/mux v1.8.0 // indirect
+	github.com/jmoiron/sqlx v1.3.4 // indirect
 )
